03eba4eeba3628e21a9a41d9c0321294759fffa4
dineshkumar-mb <dineshkumarmannibrundha@gmail.com>
2025-05-18 11:40:24 +0530

    added buttons for small screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
-// Navbar.jsx (use logout from context)
+// Navbar.jsx
 import { Link, useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../context/AuthContext';
 import { toast } from 'react-toastify';
 
 const Navbar = () => {
   const navigate = useNavigate();
   const { token, logout } = useContext(AuthContext);
+  const [isCollapsed, setIsCollapsed] = useState(true);
 
   const handleLogout = () => {
     logout();
@@ -17,7 +18,15 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
       <Link className="navbar-brand" to="/">BlogApp</Link>
-      <div className="collapse navbar-collapse">
+      <button
+        className="navbar-toggler"
+        type="button"
+        onClick={() => setIsCollapsed(!isCollapsed)}
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className={`collapse navbar-collapse ${!isCollapsed ? 'show' : ''}`}>
         <ul className="navbar-nav ms-auto">
           {token ? (
             <>
@@ -28,7 +37,7 @@ const Navbar = () => {
                 <Link className="nav-link" to="/my-blog">My Blog</Link>
               </li>
               <li className="nav-item">
-                <button className="btn btn-danger ms-2" onClick={handleLogout}>Logout</button>
+                <button className="btn btn-danger ms-lg-2 mt-2 mt-lg-0" onClick={handleLogout}>Logout</button>
               </li>
             </>
           ) : (
